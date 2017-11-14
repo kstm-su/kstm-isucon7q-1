fed45849ff3621270b0a0bed1d6b25c83c7d7b56
bgpat <bgpat@users.noreply.github.com>
2017-11-14 05:31:21 -0800

    Copy messages from mysql to redis

--- home/isucon/isubata/webapp/go/src/isubata/initialize.go
+++ home/isucon/isubata/webapp/go/src/isubata/initialize.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"os"
 	"strconv"
 
@@ -27,20 +29,37 @@ func getInitialize(c echo.Context) error {
 func resetRedis() error {
 	redisClient.FlushDB()
 
-	rows, err := db.Query("SELECT id, content FROM message")
+	messages, err := getAllMessages()
 	if err != nil {
 		return err
 	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int64
-		var content string
-		rows.Scan(&id, &content)
-		err := redisClient.Set(strconv.FormatInt(id, 10), content, 0).Err()
-		if err != nil {
+	for c, m := range messages {
+		if err := redisClient.LPush("m"+strconv.FormatInt(c, 10), m...).Err(); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func getAllMessages() ([][]byte, error) {
+	messages := make(map[int64][][]byte)
+	rows, err := db.Query("SELECT id, channel_id, user_id, content, created_at FROM message")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		m := Message{}
+		rows.Scan(&m.ID, &m.ChannelID, &m.UserID, &m.Content, &m.CreatedAt)
+		if _, exist := messages[m.ChannelID]; !exist {
+			messages[m.ChannelID] = make([][]byte, 0, 1)
+		}
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		if err := enc.Encode(m); err != nil {
+			return nil, err
+		}
+		messages[m.ChannelID] = append(messages[m.ChannelID], buf.Bytes())
+	}
+	return messages, nil
 }
