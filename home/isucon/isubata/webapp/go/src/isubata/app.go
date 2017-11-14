e9262d448e2675de83501cc7c3655bb1dbb24b7b
Goryudyuma <Goryudyuma@users.noreply.github.com>
2017-11-13 22:29:42 -0800

    Build isubata.golang

--- home/isucon/isubata/webapp/go/src/isubata/app.go
+++ home/isucon/isubata/webapp/go/src/isubata/app.go
@@ -211,6 +211,9 @@ func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM channel WHERE id > 10")
 	db.MustExec("DELETE FROM message WHERE id > 10000")
 	db.MustExec("DELETE FROM haveread")
+
+	os.Rename("/home/isucon/work", "/home/isucon/isubata/webapp/public/icons");
+
 	return c.String(204, "")
 }
 
