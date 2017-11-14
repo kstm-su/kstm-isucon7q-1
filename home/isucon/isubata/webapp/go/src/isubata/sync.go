28c3131d9fc852bf974572f78842d31311e0cee4
Your Name <you@example.com>
2017-11-14 05:38:07 -0800

    Update gitignore
    /home/isucon/isubata/webapp/go/src/isubata/sync.go

---
+++ home/isucon/isubata/webapp/go/src/isubata/sync.go
@@ -0,0 +1,32 @@
+package main
+
+func syncUser(c echo.Context) error {
+	self, err := ensureLogin(c)
+	if self == nil {
+		return err
+	}
+
+	channels := []ChannelInfo{}
+	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
+	if err != nil {
+		return err
+	}
+
+	userName := c.Param("user_name")
+	var other User
+	err = db.Get(&other, "SELECT * FROM user WHERE name = ?", userName)
+	if err == sql.ErrNoRows {
+		return echo.ErrNotFound
+	}
+	if err != nil {
+		return err
+	}
+
+	return c.Render(http.StatusOK, "profile", map[string]interface{}{
+		"ChannelID":   0,
+		"Channels":    channels,
+		"User":        self,
+		"Other":       other,
+		"SelfProfile": self.ID == other.ID,
+	})
+}
