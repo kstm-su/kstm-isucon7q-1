60fe0a089ca2eb9f4b1abda4b7f4c479a579ef9f
Goryudyuma <Goryudyuma@users.noreply.github.com>
2017-11-13 21:44:19 -0800

    画像取り出すやつ書いた

--- root/work/main.php
+++ root/work/main.php
@@ -3,7 +3,7 @@
 $pdo = new PDO('mysql:host=localhost;dbname=isubata;charset=utf8','root','',
 array(PDO::ATTR_EMULATE_PREPARES => false));
 
-$stmt = $pdo->query("SELECT * FROM テーブル名 ORDER BY no ASC");
+$stmt = $pdo->query("SELECT name, data FROM image");
 while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){
   file_put_contents($row['name'], $row['data']);
 }
