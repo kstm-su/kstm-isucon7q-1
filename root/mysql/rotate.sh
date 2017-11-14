226b92ff45c3084daeedbdb38637790ee3597c56
Goryudyuma <Goryudyuma@users.noreply.github.com>
2017-11-13 23:49:37 -0800

    rotate

--- root/mysql/rotate.sh
+++ root/mysql/rotate.sh
@@ -1,2 +1,6 @@
-rm /var
+mysqldumpslow -s t /var/log/mysql/mysql-slow.sql > mysqldumpslow.log.`date "+%s"`
+rm /var/log/mysql/mysql-slow.sql
 systemctl restart mysql
+git add .
+git add /etc/mysql/my.cnf
+git commit --allow-empty -m "rotate"
